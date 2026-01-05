8fa0ff44bd187ad07596fd7bc9115de0e4e5a4ad
Iván Moreno <ivan@ivanmz.com>
2026-01-05 12:40:42 +0100

    Child: enable sidebar on archive and category pages

--- functions.php
+++ functions.php
@@ -18,14 +18,14 @@ if ( ! function_exists( 'child_modify_posted_on_parts' ) ) {
 	add_filter( 'presscore_posted_on_parts', 'child_modify_posted_on_parts', 10 );
 }
 
-	// Force sidebar to be shown on single post pages (like home)
-	if ( ! function_exists( 'child_enable_sidebar_on_single' ) ) {
-		function child_enable_sidebar_on_single() {
-			if ( is_singular( 'post' ) ) {
-				if ( function_exists( 'presscore_config' ) ) {
-					presscore_config()->set( 'sidebar_position', 'right' );
-				}
+// Force sidebar to be shown on single post, archive and category pages
+if ( ! function_exists( 'child_enable_sidebar_on_single_and_archives' ) ) {
+	function child_enable_sidebar_on_single_and_archives() {
+		if ( function_exists( 'presscore_config' ) ) {
+			if ( is_singular( 'post' ) || is_archive() || is_category() ) {
+				presscore_config()->set( 'sidebar_position', 'right' );
 			}
 		}
-		add_action( 'get_header', 'child_enable_sidebar_on_single', 15 );
 	}
+	add_action( 'get_header', 'child_enable_sidebar_on_single_and_archives', 15 );
+}
